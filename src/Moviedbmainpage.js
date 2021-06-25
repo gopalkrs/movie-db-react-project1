a63534d59df3ea26d94d4755fda11c168a637792
Gopal Choudhary <raunakchd1@gmail.com>
2021-06-26 00:25:37 +0530

    added new background

--- src/Moviedbmainpage.js
+++ src/Moviedbmainpage.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import "./Moviedbmainpage.css";
-import Netflixsvg from "./netflix.svg";
+import Moviessvg from "./moviessvg.svg";
 import Backgroundimg from "./background.jpg";
 
 
@@ -13,7 +13,7 @@ function Moviedbmainpage() {
                 <div>
                     <h4 className="main-page-headline">Welcome, Lots of Movies and TV shows to discover here...Explore now.</h4>
                     <div className="main-page-images">
-                        <img className= "background-image" alt= "background-image-svg" src={Netflixsvg}/>
+                        <img className= "background-image" alt= "background-image-svg" src={Moviessvg}/>
                         <img className= "background-image-jpg" alt= "background-image-jpg" src={Backgroundimg}/>
                     </div>
                 </div>
