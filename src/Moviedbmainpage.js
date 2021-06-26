4207bf7f90d3fc506704967884d60fb74f254bff
Gopal Choudhary <raunakchd1@gmail.com>
2021-06-26 15:24:26 +0530

    added some styles

--- src/Moviedbmainpage.js
+++ src/Moviedbmainpage.js
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import "./Moviedbmainpage.css";
 import Moviessvg from "./moviessvg.svg";
 import Backgroundimg from "./background.jpg";
+import Videostreaming from "./Videostreaming.svg"
 
 
 function Moviedbmainpage() {
@@ -13,8 +14,11 @@ function Moviedbmainpage() {
                 <div>
                     <h4 className="main-page-headline">Welcome, Lots of Movies and TV shows to discover here...Explore now.</h4>
                     <div className="main-page-images">
-                        <img className= "background-image" alt= "background-image-svg" src={Moviessvg}/>
-                        <img className= "background-image-jpg" alt= "background-image-jpg" src={Backgroundimg}/>
+                        <div className="svg-images-main-page">
+                            <img className= "background-image" alt= "streaming-image" src={Videostreaming}/>
+                            <img className= "background-image" alt= "stream-image" src={Moviessvg}/>
+                        </div>
+                        <img className= "background-image-jpg" alt= "background-image-movies" src={Backgroundimg}/>
                     </div>
                 </div>
             </div>
